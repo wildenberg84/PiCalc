f854a488794602c1f1ced7084f0292c0570442cf
Bastiaan Wildenberg <wildenberg84@gmail.com>
2019-09-19 20:55:38 +0200

    added Nilakantha implementation

--- calcPi.py
+++ calcPi.py
@@ -1,5 +1,5 @@
 # Gregory-Leibniz implementation
-def calc(totalSteps):
+def calcGregLeib(totalSteps):
     pi = 0.0
     
     for i in range(0, totalSteps):
@@ -10,7 +10,25 @@ def calc(totalSteps):
         else:
             pi -= (4.0 / divBy)
         
-        print(pi)
+    print(pi)
 
-# note this method takes forever to converge past 3.141
-calc(100000)
+calcGregLeib(100000000)
+
+
+# Nilakantha implementation
+def calcNilak(totalSteps):
+    pi = 3.0
+    
+    for i in range(1, totalSteps):
+        
+        divBy = (i * 2) * (i * 2 + 1) * (i * 2 + 2)
+        
+        if i % 2 == 0:
+            pi -= (4.0 / divBy)
+        else:
+            pi += (4.0 / divBy)
+        
+    print(pi)
+
+
+calcNilak(100000000)
