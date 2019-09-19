47a68787bc600b5a2e622b2cc02860446516502e
Bastiaan Wildenberg <wildenberg84@gmail.com>
2019-09-19 21:09:56 +0200

    added Chudnovsky implementation

--- calcPi.py
+++ calcPi.py
@@ -12,7 +12,7 @@ def calcGregLeib(totalSteps):
         
     print(pi)
 
-calcGregLeib(100000000)
+calcGregLeib(1000000)
 
 
 # Nilakantha implementation
@@ -31,4 +31,25 @@ def calcNilak(totalSteps):
     print(pi)
 
 
-calcNilak(100000000)
+calcNilak(1000000)
+
+
+# from https://en.wikipedia.org/wiki/Chudnovsky_algorithm
+# Chudnovsky implementation
+from decimal import Decimal as Dec, getcontext as gc
+
+def calcChud(maxIter = 70, prec = 1003, disp = 1002): # parameter defaults chosen to gain 1000 digits after the comma
+    gc().prec = prec
+    K, M, L, X, S = 6, 1, 13591409, 1, 13591409
+    for k in range(1, maxIter + 1):
+        M = (K**3 - 16*K) * M // k**3 
+        L += 545140134
+        X *= -262537412640768000
+        S += Dec(M * L) / X
+        K += 12
+    pi = 426880 * Dec(10005).sqrt() / S
+    pi = Dec(str(pi)[:disp]) # drop few digits of precision for accuracy
+    print("PI(maxIter={} iterations, gc().prec={}, disp={} digits) =\n{}".format(maxIter, prec, disp, pi))
+    return pi
+
+calcChud()
